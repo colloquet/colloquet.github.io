d1e623c2eeb6255f09460f7bf083539c5c60ff56
Colloque Tsui <colloquet@gmail.com>
2014-04-11 21:48:20 +0100

    asd

--- script.js
+++ script.js
@@ -136,7 +136,6 @@ $(document).ready(function() {
 				type: "GET",
 				dataType: "jsonp",
 				headers: { 'X-Wikia-API-Key': '43af6ed19dfcc1dc9d7943d8548953529fc75fac' },
-				beforeSend: function(xhr){xhr.setRequestHeader('X-Wikia-API-Key', '43af6ed19dfcc1dc9d7943d8548953529fc75fac');},
 				success: function (data) {
 					$(".lyrics1").html("<strong>" + data.song + "<br/><span class='muted'>by " + data.artist + "</span></strong><br/><br/>" + "<iframe width='560' height='315' src='http://www.youtube.com/embed/5NV6Rdv1a3I' frameborder='0' allowfullscreen></iframe><br/><br/><p>" + data.lyrics + "</p><br/><br/><a href='" + data.url + "'>" + data.url + "</a>");
 				}
