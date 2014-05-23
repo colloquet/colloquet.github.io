90b304dd8b6c4390c27cfe3ecdc8e85da317556c
Colloque Tsui <colloquet@gmail.com>
2014-05-23 20:40:37 +0100

    kj

--- _site/script.js
+++ _site/script.js
@@ -37,4 +37,23 @@ $(document).ready(function() {
 		scrollTop: 0
 		});
 	});
+	$("#m-sidebar-btn").click(function() {
+		var sidebar = $('#m-sidebar');
+		if (sidebar.hasClass('visible')){
+			sidebar.animate({"left":"-50%"}, 350).removeClass('visible');
+		} else {
+			sidebar.animate({"left":"0px"}, 350).addClass('visible');
+		}
+		$(".overlay").fadeToggle();
+	});
+
+	$(".overlay").click(function() {
+		var sidebar = $('#m-sidebar');
+		if (sidebar.hasClass('visible')){
+			sidebar.animate({"left":"-50%"}, 350).removeClass('visible');
+		} else {
+			sidebar.animate({"left":"0px"}, 350).addClass('visible');
+		}
+		$(".overlay").fadeToggle();
+	});
 });
