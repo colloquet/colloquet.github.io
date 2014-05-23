1c4324767cf97a54abeb1d49a373f47cde4d76a8
Colloque Tsui <colloquet@gmail.com>
2014-05-23 15:04:32 +0100

    asd

--- script.js
+++ script.js
@@ -7,15 +7,6 @@ $(document).ready(function() {
 		$('#navtoport').toggleClass('currentpage');
 		$('#navtores').toggleClass('currentpage');
 	});
-	$("#m-sidebar-btn").click(function () {
-		var sidebar = $('#m-sidebar');
-		if (sidebar.hasClass('visible')){
-			sidebar.animate({"left":"-50%"}, 350).removeClass('visible');
-		} else {
-			sidebar.animate({"left":"0px"}, 350).addClass('visible');
-		}
-		$(".overlay").fadeToggle();
-	});
 	$('.tohome').click(function(e) {
 		e.preventDefault();
 		$('html, body').stop().animate({
@@ -40,46 +31,10 @@ $(document).ready(function() {
 			scrollTop: $("#contact").offset().top
 		});
 	});
-	$('#sidebar-tohome').click(function(e) {
-		e.preventDefault();
-		$('html, body').stop().animate({
-			scrollTop: $("#top").offset().top
-		});
-		var sidebar = $('#m-sidebar');
-		if (sidebar.hasClass('visible')){
-			sidebar.animate({"left":"-50%"}, 350).removeClass('visible');
-		} else {
-			sidebar.animate({"left":"0px"}, 350).addClass('visible');
-		}
-		$(".overlay").fadeOut();
-	});
-	$('#sidebar-toalbum').click(function(e) {
-		e.preventDefault();
-		$('html, body').stop().animate({
-			scrollTop: $("#myalbum").offset().top
-		});
-		var sidebar = $('#m-sidebar');
-		if (sidebar.hasClass('visible')){
-			sidebar.animate({"left":"-50%"}, 350).removeClass('visible');
-		} else {
-			sidebar.animate({"left":"0px"}, 350).addClass('visible');
-		}
-		$(".overlay").fadeOut();
-	});
 	$('.header1').click(function(e) {
 		e.preventDefault();
 		$('html, body').stop().animate({
 		scrollTop: 0
 		});
-	});
-	$('.overlay').click(function() {
-		$(".lyrics").fadeOut();
-		$(".overlay").fadeOut(function() {
-			$(".lyrics1").html("");
-		});
-		var sidebar = $('#m-sidebar');
-		if (sidebar.hasClass('visible')){
-			sidebar.animate({"left":"-50%"}, 350).removeClass('visible');
-		}
 	});
 });
