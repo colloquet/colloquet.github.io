273ec48d91523628d18186353756b64381ddc6ae
Colloque Tsui <colloquet@gmail.com>
2014-05-26 23:33:10 +0100

    asd

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
-	// $('.portfolio').waypoint(function(direction) {
-	// 	$('.navtohome').toggleClass('currentpage');
-	// 	$('.navtoport').toggleClass('currentpage');
-	// });
-	// $('.resume').waypoint(function(direction) {
-	// 	$('.navtoport').toggleClass('currentpage');
-	// 	$('.navtores').toggleClass('currentpage');
-	// });
+	$(function() {
+	    $.mobile.panel.prototype._positionPanel = function() {
+	        var self = this,
+	            panelInnerHeight = self._panelInner.outerHeight(),
+	            expand = panelInnerHeight > $.mobile.getScreenHeight();
+
+	        if ( expand || !self.options.positionFixed ) {
+	            if ( expand ) {
+	                self._unfixPanel();
+	                $.mobile.resetActivePageHeight( panelInnerHeight );
+	            }
+	          //window.scrollTo( 0, $.mobile.defaultHomeScroll );
+	        } else {
+	            self._fixPanel();
+	        }
+	    };
+	});
 	$('.tohome').click(function(e) {
 		e.preventDefault();
 		$('html, body').stop().animate({
