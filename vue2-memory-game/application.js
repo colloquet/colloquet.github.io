0907ab8f6496ec365be940662db98faa7dcc043a
Colloque Tsui <colloquet@gmail.com>
2016-08-25 00:29:39 +0800

    refactored vue memory game

--- vue2-memory-game/application.js
+++ vue2-memory-game/application.js
@@ -1,21 +1,32 @@
-var scoreBoardRef = new Firebase('https://burning-fire-4995.firebaseio.com/scores');
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAF3xxDmXjpPDrcvBj4qz58XvDywiYj26U",
+  authDomain: "memory-game-d041f.firebaseapp.com",
+  databaseURL: "https://memory-game-d041f.firebaseio.com",
+  storageBucket: "",
+};
+var memoryGame = firebase.initializeApp(config);
+var scoreBoardRef = memoryGame.database().ref('/scores');
 
 // when leadboard loaded from Firebase, we change the flag to true
 scoreBoardRef.on('value', function(snapshot) {
   Vue.set(vm, 'isScoreBoardLoaded', true);
 });
 
+// initialize Vue.js
 var vm = new Vue({
   el: '#app', // initialize Vue.js on body
   data: {
-    numberOfPair: 8, // size of the board
+    numberOfPair: 8, // size of the board (must be square number, duh!)
     numberOfMatched: 0,
     numberOfTries: 0,
     tiles: [],
     matchingArray: [], // to match 2 tiles later
     playerName: "",
     isScoreBoardLoaded: false, // to show loading status of scoreboard
-    showModal: false // determine if we should show modal
+    timeInSec: 0, // keep track of time spent
+    gameStarted: false,
+    timer: null
   },
   firebase: {
     // get leaderboard from Firebase
@@ -29,15 +40,27 @@ var vm = new Vue({
       });
     },
     computedScore: function() {
-      return Math.round((this.numberOfMatched / this.numberOfTries) * 100);
+      return Math.round((this.numberOfMatched / this.numberOfTries) * 100); // this equation determine final score
     },
     sortedScoreBoard: function() {
       return this.scoreBoard.sort(function(a, b){
-        return a.score < b.score;
+        // if same score, we rank player with less time higher
+        if (a.score == b.score) {
+          return (b.time < a.time) ? 1 : (b.time > a.time) ? -1 : 0;
+        }
+
+        return (b.score > a.score) ? 1 : (b.score < a.score) ? -1 : 0;
       });
     },
     computedTileClass: function() {
       return 'uk-width-1-' + Math.sqrt(this.numberOfPair*2);
+    },
+    computedTime: function() {
+      // convert seconds to mm:ss format
+      var minutes = parseInt( this.timeInSec / 60 ) % 60;
+      var seconds = this.timeInSec % 60;
+
+      return (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);
     }
   },
   methods: {
@@ -52,7 +75,8 @@ var vm = new Vue({
       if (self.playerName !== "") {
         self.$firebaseRefs.scoreBoard.push({
           name: self.playerName,
-          score: self.computedScore // make sure it's an interger not string
+          score: self.computedScore, // make sure it's an interger not string
+          time: self.timeInSec
         });
       }
 
@@ -62,13 +86,14 @@ var vm = new Vue({
         tile.matched = false;
       });
 
-      // reset state after 500ms
+      // reset state after 500ms (wait till all tiles flipped back)
       setTimeout(function() {
         self.numberOfMatched = 0;
         self.numberOfTries = 0;
         self.tiles = [];
         self.matchingArray = [];
         self.playerName = "";
+        self.resetTimer();
         self.generateTiles();
       }, 500);
     },
@@ -105,6 +130,11 @@ var vm = new Vue({
       });
     },
     toggleTile: function(tile) {
+      if (!this.gameStarted) {
+        this.gameStarted = true;
+        this.startTimer();
+      }
+
       // there should be no more than 2 tiles when we compare
       if (this.matchingArray.length >= 2) {
         return;
@@ -112,8 +142,6 @@ var vm = new Vue({
 
       // do nothing if user click on same tile twice
       if (this.matchingArray[0] == tile) {
-        // tile.flipped = !tile.flipped;
-        // this.matchingArray = [];
         return;
       }
 
@@ -154,12 +182,26 @@ var vm = new Vue({
         }, 500);
       }
 
-    }
-  },
-  watch: {
-    // if game is over, show modal
-    'isGameFinished': function (val, oldVal) {
-      this.showModal = val;
+      self.$nextTick(function() {
+        if (self.isGameFinished) {
+          self.gameStarted = false;
+          this.stopTimer();
+        }
+      });
+
+    },
+    startTimer: function() {
+      var self = this;
+
+      self.timer = setInterval(function() {
+        self.timeInSec++;
+      }, 1000);
+    },
+    stopTimer: function() {
+      clearInterval(this.timer);
+    },
+    resetTimer: function() {
+      this.timeInSec = 0;
     }
   },
   mounted: function() {
